597e69b65422d6ab289df44e92e4fea9ffdca71f
MarkCiliaVincenti <markciliavincenti@gmail.com>
2017-01-23 19:55:57 +0100

    Movies played by logged in user will now be hidden from channel.

--- MediaBrowser.Channels.BlurN/BlurNChannel.cs
+++ MediaBrowser.Channels.BlurN/BlurNChannel.cs
@@ -24,12 +24,16 @@ namespace MediaBrowser.Channels.BlurN
         private readonly IJsonSerializer _json;
         private readonly IApplicationPaths _appPaths;
         private readonly IFileSystem _fileSystem;
+        private readonly ILibraryManager _libraryManager;
+        private readonly IUserManager _userManager;
 
-        public BlurNChannel(IJsonSerializer json, IApplicationPaths appPaths, IFileSystem fileSystem)
+        public BlurNChannel(IUserManager userManager, ILibraryManager libraryManager, IJsonSerializer json, IApplicationPaths appPaths, IFileSystem fileSystem)
         {
             _json = json;
             _appPaths = appPaths;
             _fileSystem = fileSystem;
+            _userManager = userManager;
+            _libraryManager = libraryManager;
         }
 
 
@@ -125,6 +129,18 @@ namespace MediaBrowser.Channels.BlurN
             if (debug)
                 Plugin.Logger.Debug("Entered BlurN channel list");
 
+            User user = _userManager.GetUserById(query.UserId);
+
+            IEnumerable<BaseItem> library;
+            library = _libraryManager.GetItemList(new InternalItemsQuery() { HasImdbId = true, User = user, IsPlayed = true, SourceTypes = new SourceType[] { SourceType.Library } });
+            Dictionary<string, BaseItem> libDict = new Dictionary<string, BaseItem>();
+            foreach (BaseItem libItem in library)
+            {
+                string libIMDbId = libItem.GetProviderId(MetadataProviders.Imdb);
+                if (!libDict.ContainsKey(libIMDbId))
+                    libDict.Add(libIMDbId, libItem);
+            }
+
             OMDBList items = new OMDBList();
             if (Plugin.Instance.Configuration.Items.List.Count > 0)
                 items.List = Plugin.Instance.Configuration.Items.List;
@@ -173,58 +189,68 @@ namespace MediaBrowser.Channels.BlurN
 
             ChannelItemResult result = new ChannelItemResult() { TotalRecordCount = items.List.Count };
 
-            if (debug)
-                Plugin.Logger.Debug("Set total record count ({0})", (int)result.TotalRecordCount);
-
             for (int i = 0; i < showList.List.Count; i++)
             {
                 OMDB omdb = showList.List[i];
 
-                if (debug)
-                    Plugin.Logger.Debug("Showing movie '{0}' to BlurN channel list", omdb.Title);
-
-                List<string> directors = (string.IsNullOrEmpty(omdb.Director)) ? new List<string>() : omdb.Director.Replace(", ", ",").Split(',').ToList();
-                List<string> writers = (string.IsNullOrEmpty(omdb.Writer)) ? new List<string>() : omdb.Writer.Replace(", ", ",").Split(',').ToList();
-                List<string> actors = (string.IsNullOrEmpty(omdb.Actors)) ? new List<string>() : omdb.Actors.Replace(", ", ",").Split(',').ToList();
-
-                List<PersonInfo> people = new List<PersonInfo>();
-                foreach (string director in directors)
-                    people.Add(new PersonInfo() { Name = director, Role = "Director" });
-                foreach (string writer in writers)
-                    people.Add(new PersonInfo() { Name = writer, Role = "Writer" });
-                foreach (string actor in actors)
-                    people.Add(new PersonInfo() { Name = actor, Role = "Actor" });
-
-                List<string> genres = (string.IsNullOrEmpty(omdb.Genre)) ? new List<string>() : omdb.Genre.Replace(", ", ",").Split(',').ToList();
-
-                var cii = new ChannelItemInfo()
+                if (libDict.ContainsKey(omdb.ImdbId))
                 {
-                    Id = Plugin.Instance.PluginConfiguration.ChannelRefreshCount.ToString() + "-" + omdb.ImdbId,
-                    IndexNumber = i,
-                    CommunityRating = (float)omdb.ImdbRating,
-                    ContentType = ChannelMediaContentType.Movie,
-                    DateCreated = omdb.BluRayReleaseDate,
-                    Genres = genres,
-                    ImageUrl = (omdb.Poster == "N/A") ? null : omdb.Poster,
-                    MediaType = ChannelMediaType.Video,
-                    Name = omdb.Title,
-                    OfficialRating = (omdb.Rated == "N/A") ? null : omdb.Rated,
-                    Overview = (omdb.Plot == "N/A") ? null : omdb.Plot,
-                    People = people,
-                    PremiereDate = omdb.Released,
-                    ProductionYear = omdb.Released.Year,
-                    RunTimeTicks = (string.IsNullOrEmpty(omdb.Runtime) || (omdb.Runtime == "N/A")) ? null : (long?)TimeSpan.FromMinutes(Convert.ToInt32(omdb.Runtime.Split(' ')[0])).Ticks,
-                    Type = ChannelItemType.Media,
-                    IsInfiniteStream = false
-                };
+                    result.TotalRecordCount--;
 
-                cii.SetProviderId(MetadataProviders.Imdb, omdb.ImdbId);
+                    if (debug)
+                        Plugin.Logger.Debug("Hiding movie '{0}' from BlurN channel list as watched by user", omdb.Title);
+                }
+                else
+                {
+                    if (debug)
+                        Plugin.Logger.Debug("Showing movie '{0}' to BlurN channel list", omdb.Title);
 
-                result.Items.Add(cii);
+                    List<string> directors = (string.IsNullOrEmpty(omdb.Director)) ? new List<string>() : omdb.Director.Replace(", ", ",").Split(',').ToList();
+                    List<string> writers = (string.IsNullOrEmpty(omdb.Writer)) ? new List<string>() : omdb.Writer.Replace(", ", ",").Split(',').ToList();
+                    List<string> actors = (string.IsNullOrEmpty(omdb.Actors)) ? new List<string>() : omdb.Actors.Replace(", ", ",").Split(',').ToList();
 
-                if (debug)
-                    Plugin.Logger.Debug("Added movie '{0}' to BlurN channel list", omdb.Title);
+                    List<PersonInfo> people = new List<PersonInfo>();
+                    foreach (string director in directors)
+                        people.Add(new PersonInfo() { Name = director, Role = "Director" });
+                    foreach (string writer in writers)
+                        people.Add(new PersonInfo() { Name = writer, Role = "Writer" });
+                    foreach (string actor in actors)
+                        people.Add(new PersonInfo() { Name = actor, Role = "Actor" });
+
+                    List<string> genres = (string.IsNullOrEmpty(omdb.Genre)) ? new List<string>() : omdb.Genre.Replace(", ", ",").Split(',').ToList();
+
+                    var cii = new ChannelItemInfo()
+                    {
+                        Id = Plugin.Instance.PluginConfiguration.ChannelRefreshCount.ToString() + "-" + omdb.ImdbId,
+                        IndexNumber = i,
+                        CommunityRating = (float)omdb.ImdbRating,
+                        ContentType = ChannelMediaContentType.Movie,
+                        DateCreated = omdb.BluRayReleaseDate,
+                        Genres = genres,
+                        ImageUrl = (omdb.Poster == "N/A") ? null : omdb.Poster,
+                        MediaType = ChannelMediaType.Video,
+                        Name = omdb.Title,
+                        OfficialRating = (omdb.Rated == "N/A") ? null : omdb.Rated,
+                        Overview = (omdb.Plot == "N/A") ? null : omdb.Plot,
+                        People = people,
+                        PremiereDate = omdb.Released,
+                        ProductionYear = omdb.Released.Year,
+                        RunTimeTicks = (string.IsNullOrEmpty(omdb.Runtime) || (omdb.Runtime == "N/A")) ? null : (long?)TimeSpan.FromMinutes(Convert.ToInt32(omdb.Runtime.Split(' ')[0])).Ticks,
+                        Type = ChannelItemType.Media,
+                        IsInfiniteStream = false
+                    };
+
+                    cii.SetProviderId(MetadataProviders.Imdb, omdb.ImdbId);
+
+                    result.Items.Add(cii);
+
+                    if (debug)
+                        Plugin.Logger.Debug("Added movie '{0}' to BlurN channel list", omdb.Title);
+                }
             }
+
+            if (debug)
+                Plugin.Logger.Debug("Set total record count ({0})", (int)result.TotalRecordCount);
 
             return Task.FromResult(result);
         }
